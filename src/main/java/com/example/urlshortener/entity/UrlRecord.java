d12d42b5b4cbe1859cba6c197ec1f0c0e1b69e1a
Sahil Naik <nsahil992@gmail.com>
2026-02-27 21:00:40 +0530

    feat: add UrlRecord JPA entity

---
+++ src/main/java/com/example/urlshortener/entity/UrlRecord.java
@@ -0,0 +1,33 @@
+package com.example.urlshortener.entity;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "url_records")
+public class UrlRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull(message = "Original URL cannot be blank")
+    @Column(name = "original_url", nullable = false)
+    private String originalUrl;
+
+    @Column(name = "short_code", unique = true, nullable = false, length = 10)
+    private String shortCode;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+}
